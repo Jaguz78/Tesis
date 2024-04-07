91dbb6e87f187cdff2099bff1e8ff9e7c8cfc634
amarielos <marag41639@gmail.com>
2024-04-07 14:51:18 -0600

    maquetacion y estilos navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,9 +1,38 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
-export default function navbar() {
+export default function Navbar() {
   return (
-    <div>
-      
+    <div className="nav bg-zinc-900 py-5 flex text-gray-400 place-content-between font-bold text-lg">
+
+      <ul className="flex mx-5 space-x-7 ">       
+        <li>
+          <NavLink to='/' style={({ isActive }) => {
+          return isActive ? { color: "white" } : {};}}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/documento' style={({ isActive }) => {
+          return isActive ? { color: "white" } : {};}} >Documento</NavLink>
+        </li>
+        <li>
+          <NavLink to='/usuario' style={({ isActive }) => {
+          return isActive ? { color: "white" } : {};}}>Usuarios</NavLink>
+        </li>
+      </ul>
+
+
+      <ul className="flex mx-5 space-x-7">
+        <li>
+          <NavLink to='/cambiarContraseña' style={({ isActive }) => {
+          return isActive ? { color: "white" } : {};}}>Cambiar Contraseña</NavLink>
+        </li>
+        <li>
+          <button className="px-2 py-1 bg-sky-500/75 text-white rounded-lg">
+          <NavLink to='/login'>Log in</NavLink>
+          </button>
+        </li>
+      </ul>
+
+
     </div>
   )
 }
