824c13d7e4d2983b3fdd99c1f2fb928f047fc137
Alejandro Guzmán <jalejandrogh78@gmail.com>
2024-04-07 13:34:53 -0600

    Funcionamiento de los botones de formato

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,24 @@
-
+import '../style.css'
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <div className="w-96 p-4 border-2 rounded-xl">
+        <span className="title">Titulo</span>
+      </div>
+      <div className="w-96 p-4 border-2 rounded-xl">
+        <span className="title2">Titulo 2 Prro</span>
+      </div>
+      <div className="w-96 p-4 border-2 rounded-xl">
+        <span className="title3">Titlo 3 Pa</span>
+      </div>
+      <div className="w-96 p-4 border-2 rounded-xl">
+        <span className="title4">Titlo 4 lorem ipsum</span>
+      </div>
+      <div className="w-96 p-4 border-2 rounded-xl">
+        <span className="paragraph">Parrafo de prueba</span>
+      </div>
     </div>
+    
   )
 }
 
