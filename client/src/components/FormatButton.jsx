676a27b1161404a5c7b9209cc70891681f5e4652
Alejandro Guzmán <jalejandrogh78@gmail.com>
2024-04-07 15:05:31 -0600

    Funcionamiento de los botones y validación de datos

--- client/src/components/FormatButton.jsx
+++ client/src/components/FormatButton.jsx
@@ -1,12 +1,16 @@
 import '../style.css'
 import React from 'react'
 
-export default function FormatButton({children, typeFormat, setFormat}) {
+export default function FormatButton({children, typeFormat, setFormat, activeButton}) {
 
-const handleClic = e => setFormat(typeFormat)
+  const handleClic = e => {
+    setFormat(typeFormat)
+  }
 
   return (
-    <button onClick={handleClic} className='w-14 h-14 bg-transparent border-2 shadow-inner border-slate-800 rounded-lg font-bold mr-4 focus:bg-slate-400'>{children}</button>
+    <button onClick={handleClic} className={`w-14 h-14 bg-transparent border-2 shadow-inner border-slate-800 rounded-lg font-bold mr-4 ${activeButton === typeFormat ? 'bg-slate-400': 'bg-transparent'}`}>
+      {children}
+      </button>
   )
 }
 
--- client/src/pages/Documento.jsx
+++ client/src/pages/Documento.jsx
@@ -4,15 +4,49 @@ import {useState} from 'react'
 
 function Documento() {
     const [format, setFormat] = useState('');
+    const [data, setData] = useState('')
+
+    const validateData = _ => {
+        let response = {
+            data: data.trim(),
+        }
+        if (format === 'title' || format === 'title2' || format === 'title3' || format === 'title4') {
+            if (data.length >= 48)  response.error = "Muy largo"
+        }
+
+        if (format === 'title2' || format === 'title3' || format === 'title4') {
+            response.data = data.charAt(0).toUpperCase() + data.slice(1).toLowerCase()
+        }
+
+        if (format === 'paragraph'){
+            const parrafos = data.split('\n');
+            const parrafosCapitalizados = parrafos.map(parrafo => {
+                // Si el párrafo está vacío, regresamos una cadena vacía
+                if (!parrafo.trim()) return "";
+            
+                    if (data.length <= 350) response.error = "Demasiado corto"
+                    if (data.length >= 850) response.error = "Demasiado largo"
+
+                // Capitalizamos la primera letra del párrafo y la unimos con el resto de la cadena en minúsculas
+                return parrafo.charAt(0).toUpperCase() + parrafo.slice(1).toLowerCase();
+              });
+              response.data = parrafosCapitalizados
+        }
+
+        if (format === '') response.error = "Debe seleccionar un formato"
+        console.log(response)
+        return response
+    }
+
     return (
         <main className='w-[90vw] flex flex-col m-auto mt-4'>
             <section className='w-full flex flex-row justify-between'>
                 <div className='flex flex-row'>
-                    <FormatButton setFormat={setFormat} typeFormat='title'>T</FormatButton>
-                    <FormatButton setFormat={setFormat} typeFormat='title2'>T2</FormatButton>
-                    <FormatButton setFormat={setFormat} typeFormat='title3'>T3</FormatButton>
-                    <FormatButton setFormat={setFormat} typeFormat='title4'>T4</FormatButton>
-                    <FormatButton setFormat={setFormat} typeFormat='paragraph'>P</FormatButton>
+                    <FormatButton setFormat={setFormat} typeFormat='title' activeButton={format}>T</FormatButton>
+                    <FormatButton setFormat={setFormat} typeFormat='title2' activeButton={format}>T2</FormatButton>
+                    <FormatButton setFormat={setFormat} typeFormat='title3' activeButton={format}>T3</FormatButton>
+                    <FormatButton setFormat={setFormat} typeFormat='title4' activeButton={format}>T4</FormatButton>
+                    <FormatButton setFormat={setFormat} typeFormat='paragraph' activeButton={format}>P</FormatButton>
                 </div>
                 <div>
                     <button className='p-3 bg-my-green text-white rounded-lg'>Imprimir PDF</button>
@@ -20,8 +54,8 @@ function Documento() {
             </section>
             <section className='mt-4 w-full h-[75vh] flex flex-row justify-between'>
                 <div className='w-[48%] h-full border-2 border-gray-600 rounded-2xl p-4 flex flex-col shadow-xl'>
-                    <textarea name="inputTextarea" id="inputTextarea" className='w-full h-[90%] rounded-lg p-2 border-0 focus:outline-none' placeholder='Escribe aquí...'></textarea>
-                    <button className='w-24 p-3 bg-my-green text-white rounded-lg mt-4 self-end'>Agregar</button>
+                    <textarea onChange={(e) => setData(e.target.value)} name="inputTextarea" id="inputTextarea" className='w-full h-[90%] rounded-lg p-2 border-0 focus:outline-none' placeholder='Escribe aquí...'></textarea>
+                    <button onClick={validateData} className='w-24 p-3 bg-my-green text-white rounded-lg mt-4 self-end'>Agregar</button>
                 </div>
                 <div className='w-[48%] h-full border-2 border-gray-600 rounded-2xl p-4 flex flex-col shadow-xl'>
 
