077301115737d24158e7d7e53dbd391b6519f6d6
amarielos <marag41639@gmail.com>
2024-04-20 15:13:39 -0600

    NavElement y Seccion creados

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,17 @@
 import { NavLink } from "react-router-dom"
+import React from "react"
+import NavElement from "./NavElement";
 
 export default function Navbar() {
   return (
     <div className="nav bg-zinc-900 py-5 flex text-gray-400 place-content-between font-bold text-lg">
-
       <ul className="flex mx-5 space-x-7 ">       
-        <li>
-          <NavLink to='/' style={({ isActive }) => {
-          return isActive ? { color: "white" } : {};}}>Home</NavLink>
-        </li>
-        <li>
-          <NavLink to='/documento' style={({ isActive }) => {
-          return isActive ? { color: "white" } : {};}} >Documento</NavLink>
-        </li>
-        <li>
-          <NavLink to='/usuario' style={({ isActive }) => {
-          return isActive ? { color: "white" } : {};}}>Usuarios</NavLink>
-        </li>
+        <NavElement nombre={'Home'} ruta={'./'}/>
+        <NavElement nombre={'Documento'} ruta={'/documento'} />
+        <NavElement nombre={'Usuarios'} ruta={'/usuario'} />
       </ul>
-
       <ul className="flex mx-5 space-x-7">
-        <li className="text-sm py-1.5">
-          <NavLink to='/cambiarContraseña' style={({ isActive }) => {
-          return isActive ? { color: "white" } : {};}}>Cambiar Contraseña</NavLink>
-        </li>
+        <NavElement clase="text-sm py-1.5" nombre={'Cambiar Contraseña' } ruta={'/cambiarContraseña'}/>
         <li>
           <button className="px-2 py-1 bg-sky-500/75 text-white rounded-lg">
           <NavLink to='/login'>Log in</NavLink>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,21 +2,7 @@ import '../style.css'
 export default function Home() {
   return (
     <div>
-      <div className="w-96 p-4 border-2 rounded-xl">
-        <span className="title">Titulo</span>
-      </div>
-      <div className="w-96 p-4 border-2 rounded-xl">
-        <span className="title2">Titulo 2 Prro</span>
-      </div>
-      <div className="w-96 p-4 border-2 rounded-xl">
-        <span className="title3">Titlo 3 Pa</span>
-      </div>
-      <div className="w-96 p-4 border-2 rounded-xl">
-        <span className="title4">Titlo 4 lorem ipsum</span>
-      </div>
-      <div className="w-96 p-4 border-2 rounded-xl">
-        <span className="paragraph">Parrafo de prueba</span>
-      </div>
+      
     </div>
     
   )
