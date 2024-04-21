1dc4c939ca351f7a15e6841671b678d53aa21c33
amarielos <marag41639@gmail.com>
2024-04-21 16:16:42 -0600

    front de Login y Cambiar Contraseña

--- client/src/pages/CambiarContraseña.jsx
+++ client/src/pages/CambiarContraseña.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import InputField from '../components/InputField'
 
 export default function CambiarContraseña() {
   return (
-    <div>
-      <h1>cambiarContraseña</h1>
+    <div className='my-7 m-auto w-[30%] h-full border-2 border-gray-600 rounded-2xl p-4 shadow-xl flex' >
+
+      <form className='w-[90%] m-auto block '>
+
+        <InputField id={'carné'} type={'number'} name={'Carné'} placeholder={'Ingrese su carné...'} margin={'mb-5'}/>
+
+        <InputField id={'contraseñaActual'} type={'password'} name={'Contraseña Actual'} placeholder={'Ingrese su contarseña actual...'} margin={'mb-5'}/>
+
+        <InputField id={'contraseñaNueva'} type={'password'} name={'Contraseña Nueva'} placeholder={'Ingrese su nueva contraseña...'} margin={'mb-5'}/>
+
+        <InputField id={'contraseñaNuevaRep'} type={'password'} name={'Repita su Contraseña'} placeholder={'Repita su contraseña...'} margin={'mb-5'}/>
+        
+        <button 
+        className='my-3 w-[100%] p-2 bg-my-green text-white rounded-lg '>Cambiar Contraseña</button> 
+      </form>
+      
     </div>
   )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import InputField from '../components/InputField'
 
 export default function Login() {
   return (
-    <div>
-      <h1>login</h1>
+    <div className='mt-10 mb-10 m-auto w-[30%] h-full border-2 border-gray-600 rounded-2xl p-4 shadow-xl flex' >
+      <form className='w-[90%] m-auto block '>
+        <InputField id={'carné'} type={'number'} name={'Carné'} placeholder={'Ingrese su carné...'} margin={'my-10'}/>
+        <InputField id={'contraseña'} type={'password'} name={'Contraseña'} placeholder={'Ingrese su contraseña...'} margin={'my-10'}/>
+        
+        <button 
+        className='mb-10 mt-10 w-[100%] p-2 bg-my-green text-white rounded-lg '>Iniciar Sesión</button> 
+      </form>
+      
     </div>
   )
 }
