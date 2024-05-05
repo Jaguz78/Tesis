4425177e3ac3e42d804bcf64f867ff8a615d2c72
Anthony Gonzalez <gonzalezanthony0320@gmail.com>
2024-05-05 08:58:36 -0600

    Ajustes a la base de datos

--- database/db.sql
+++ database/db.sql
@@ -1,16 +1,21 @@
 create database if not exists proyecto;
 use proyecto;
+create table if not exists junta(
+	id_junta int primary key auto_increment,
+    fecha date
+);
+create table if not exists detalle_junta(
+	id_detalle_junta int primary key auto_increment,
+    id_junta int,
+    grupo varchar(250),
+    puesto varchar(250),
+    nombre varchar(250),
+    constraint `FK_DetalleJunta` foreign key (`id_junta`) references `junta` (`id_junta`)
+);
 create table if not exists usuario(
 	id_usuario varchar(50) primary key,
     contraseña varchar(100),
     rol varchar(100)
-);
-create table if not exists asesor(
-	id_asesor int primary key,
-    nombre varchar(100),
-    id_usuario varchar(50), 
-    state boolean,
-    constraint `FK_AsesorUsuario` foreign key (`id_usuario`) references `usuario` (`id_usuario`)
 );
 create table if not exists estudiante(
 	id_estudiante int primary key,
@@ -27,14 +32,9 @@ create table if not exists tesis(
     enlace_pdf text,
     id_estudiante int,
     state boolean,
-    constraint `FK_TesisUsuario` foreign key (`id_estudiante`) references `estudiante` (`id_estudiante`)
-);
-create table if not exists tesis_asesor(
-	id_tesis_asesor int primary key auto_increment,
-    id_tesis int,
-    id_asesor int,
-    constraint `FK_TesisAsesor1` foreign key (`id_tesis`) references `tesis` (`id_tesis`),
-    constraint `FK_TesisAsesor2` foreign key (`id_asesor`) references `asesor` (`id_asesor`)
+    id_junta int,
+    constraint `FK_TesisUsuario` foreign key (`id_estudiante`) references `estudiante` (`id_estudiante`),
+    constraint `FK_TesisJunta` foreign key (`id_junta`) references `junta` (`id_junta`)
 );
 create table if not exists formato(
 	id_formato int primary key auto_increment,
@@ -60,14 +60,6 @@ create table if not exists tesis_seccion(
     constraint `FK_TesisSeccion1` foreign key (`id_tesis`) references `tesis` (`id_tesis`),
 	constraint `FK_TesisSeccion2` foreign key (`id_seccion`) references `seccion` (`id_seccion`)
 );
-insert into usuario (id_usuario, contraseña, rol) values
-	('admin', 'admin123', 'administrador'),
-    ('202146223', 'mani0320', 'estudiante'),
-	('202045889', 'juanito60', 'asesor');
-insert into asesor (id_asesor, nombre, id_usuario, state) values
-	(1, 'Juan Aguilar', '202045889', true);
-insert into estudiante (id_estudiante, id_usuario, sede, nombre, carrera) values
-	(1, '202146223', 'Centro Universitario del Norte', 'Edgar Gonzalez', 'Ingenierira en Ciencias y Sistemas');
 insert into formato (nombre_seccion, tamaño_fuente, mayuscula, cursiva, negrita, alineacion, max_caracteres) values
 	('Titulo 1', 14, true, false, true, 'centrado', 48),
     ('Titulo 2', 14, false, false, true, 'izquierda', 48),
